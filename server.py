12be6bf4f8bf40407dd31f106f715e17f1554653
Matthew Knight <knightpmatthew@gmail.com>
2019-03-08 21:53:12 +1300

    TinyDB integration, post/get with randomisation done

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
+from tinydb import TinyDB
+import random
 
 app = Flask(__name__)
-
+thunkDB = TinyDB('thunks.json')
 
 @app.route("/thunk", methods=['GET', 'POST'])
 def processThunk():
     if request.method == "GET":
-        return "SENDING THUNK"
+        return random.choice(thunkDB.all())['thunkContent']
 
     elif request.method == "POST":
-        return "INSERTING NEW THUNK"
+        thunkDB.insert(request.get_json())
+        return 'Inserted thunk'
